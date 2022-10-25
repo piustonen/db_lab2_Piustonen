344735157a7354c890cc4e1379489c9b636d08d3
piustonen <72132818+piustonen@users.noreply.github.com>
2022-10-25 19:00:48 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+import psycopg2
+
+username = 'Piustonen'
+password = '2010'
+database = 'Piustonen01_DB'
+host = 'localhost'
+port = '5432'
+
+query_1 = '''
+SELECT TRIM(artist_name) as artists, 
+COUNT(artist_id) FROM song_release 
+JOIN artists USING(artist_id) 
+GROUP BY artist_name
+ORDER BY COUNT DESC
+'''
+
+query_2 = '''
+SELECT TRIM(genre_name) as genres, 
+COUNT(genre_id) FROM song_release 
+JOIN genres USING(genre_id) 
+GROUP BY genre_name
+'''
+
+query_3 = '''
+SELECT song_name, tempo
+FROM songs
+WHERE tempo > 100
+'''
+
+conn = psycopg2.connect(user = username, password = password, dbname = database, host = host, port = port)
+
+with conn:
+    print("Database opened successfully")
+    cur = conn.cursor()
+
+    print('1.\n')
+    cur.execute(query_1)
+    for row in cur:
+        print(row)
+
+    print('2.\n')
+    cur.execute(query_2)
+    for row in cur:
+        print(row)
+
+    print('3.\n')
+    cur.execute(query_3)
+    for row in cur:
+        print(row)
+
