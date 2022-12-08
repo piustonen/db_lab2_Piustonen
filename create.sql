e6eca378e80ee0d2fa3482e08ded01beed3b470c
piustonen <72132818+piustonen@users.noreply.github.com>
2022-12-08 14:35:39 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,37 +1,51 @@
-CREATE TABLE songs(
-	song_id int NOT NULL,
-	song_name char(50) NOT NULL,
-	loudness int NOT NULL,
-	speechiness int NOT NULL,
-	tempo int NOT NULL,
-	danceability int NOT NULL);
+CREATE TABLE cloud_genres
+(
+  genre_id      char(10)  NOT NULL ,
+  genre_name    char(50)  NOT NULL 
+);
+CREATE TABLE cloud_songs
+(
+  song_id      char(10)  NOT NULL ,
+  song_name    char(50)  NOT NULL 
+);
+CREATE TABLE cloud_artists
+(
+  artist_id      char(10)  NOT NULL ,
+  artist_name    char(50)  NOT NULL 
+);
+CREATE TABLE cloud_releases
+(
+  release_id      char(10)  NOT NULL ,
+  genre_id        char(10)  NOT NULL ,
+  song_id         char(10)  NOT NULL ,
+  release_date    date      NOT NULL ,
+  release_time    time      NULL     ,
+  release_place   char(50)  NULL
+);
 
-CREATE TABLE genres(
-	genre_id int NOT NULL ,
-	genre_name char(50)  NOT NULL);
+CREATE TABLE cloud_performances
+(
+  perf_id        char(10)   NOT NULL ,
+  artist_id      char(10)   NOT NULL ,
+  song_id        char(10)   NOT NULL ,
+  perf_date      date       NOT NULL ,
+  perf_time      time       NULL     ,
+  perf_place     char(50)   NULL 
+);
 
-CREATE TABLE artists(
-	artist_id int NOT NULL,
-	artist_name char(50) NOT NULL);
-
-CREATE TABLE song_release(
-	song_release_id int NOT NULL,
-	artist_id int NOT NULL,
-	song_id int NOT NULL,
-	genre_id int NOT NULL);
-
-ALTER TABLE songs ADD CONSTRAINT PK_songs PRIMARY KEY (song_id);
-ALTER TABLE genres ADD CONSTRAINT PK_genres PRIMARY KEY (genre_id);
-ALTER TABLE artists ADD CONSTRAINT PK_artists PRIMARY KEY (artist_id);
-ALTER TABLE song_release ADD CONSTRAINT PK_song_release PRIMARY KEY (song_release_id);
-
-ALTER TABLE song_release
-ADD CONSTRAINT FK_song_release_artists FOREIGN KEY (artist_id) REFERENCES artists (artist_id);
-
-ALTER TABLE song_release
-ADD CONSTRAINT FK_song_release_songs FOREIGN KEY (song_id) REFERENCES songs (song_id);
-
-ALTER TABLE song_release
-ADD CONSTRAINT FK_song_release_genres FOREIGN KEY (genre_id) REFERENCES genres (genre_id);
+ALTER TABLE cloud_genres ADD CONSTRAINT PK_cloud_genres PRIMARY KEY (genre_id);
+ALTER TABLE cloud_songs ADD CONSTRAINT PK_cloud_songs PRIMARY KEY (song_id);
+ALTER TABLE cloud_artists ADD CONSTRAINT PK_cloud_artists PRIMARY KEY (artist_id);
+ALTER TABLE cloud_releases ADD CONSTRAINT PK_cloud_releases PRIMARY KEY (release_id);
+ALTER TABLE cloud_performances ADD CONSTRAINT PK_cloud_performances PRIMARY KEY (perf_id);
 
 
+ALTER TABLE cloud_releases
+ADD CONSTRAINT FK_cloud_releases_cloud_genres FOREIGN KEY (genre_id) REFERENCES cloud_genres (genre_id);
+ALTER TABLE cloud_releases
+ADD CONSTRAINT FK_cloud_releases_cloud_songs FOREIGN KEY (song_id) REFERENCES cloud_songs (song_id);
+
+ALTER TABLE cloud_performances
+ADD CONSTRAINT FK_cloud_performances_cloud_artists FOREIGN KEY (artist_id) REFERENCES cloud_artists (artist_id);
+ALTER TABLE cloud_performances
+ADD CONSTRAINT FK_cloud_performances_cloud_songs FOREIGN KEY (song_id) REFERENCES cloud_songs (song_id);
